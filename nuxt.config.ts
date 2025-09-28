8580266d9b2818d81e977c43665c80caba1e0675
suraj-aviato <suraj@aviato.consulting>
2025-09-28 11:20:33 +0530

    Package installtion for BE

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -2,5 +2,5 @@
 export default defineNuxtConfig({
   compatibilityDate: '2025-07-15',
   devtools: { enabled: true },
-  modules: ['@nuxtjs/tailwindcss', '@pinia/nuxt']
+  modules: ['@nuxtjs/tailwindcss', '@pinia/nuxt',"@prisma/nuxt"]
 })
