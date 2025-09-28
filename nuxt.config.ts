695e9ab81f226106ba562a0bae343d33223420f7
suraj-aviato <suraj@aviato.consulting>
2025-09-28 11:45:00 +0530

    Issue resolved

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -2,5 +2,18 @@
 export default defineNuxtConfig({
   compatibilityDate: '2025-07-15',
   devtools: { enabled: true },
-  modules: ['@nuxtjs/tailwindcss', '@pinia/nuxt',"@prisma/nuxt"]
+  components:[{
+    path:'~/components',
+    pathPrefix:false
+  }],
+  modules: ['@nuxtjs/tailwindcss', '@pinia/nuxt',"@prisma/nuxt"],
+  runtimeConfig:{
+    // PRIVATE
+    JWT_TOKEN_KEY: process.env.JWT_TOKEN_KEY,
+    REFRESH_TOKEN_KEY: process.env.REFRESH_TOKEN_KEY,
+    DATABASE_URL: process.env.DATABASE_URL,
+    public:{
+      // PUBLIC
+    }
+  }
 })
