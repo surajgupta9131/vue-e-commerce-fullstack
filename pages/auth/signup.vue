e42563b00446a8cc96f4b6c784f5a918a057ddc7
suraj-aviato <suraj@aviato.consulting>
2025-09-28 11:59:52 +0530

    Register flow api is done

--- pages/auth/signup.vue
+++ pages/auth/signup.vue
@@ -2,7 +2,6 @@
 import { useVuelidate } from "@vuelidate/core";
 import { required, email } from "@vuelidate/validators";
 
-
 definePageMeta({
   layout: "auth",
 });
@@ -16,6 +15,7 @@ const registerInput = ref({
   email: "",
   password: "",
 });
+const loading = ref(false);
 const v$ = useVuelidate(rules, registerInput);
 async function submitInput() {
   const isValid = v$.value.$validate();
@@ -23,35 +23,55 @@ async function submitInput() {
     return;
   }
 
+  loading.value = true;
+  try {
+    const res = await $fetch("/api/auth/register", {
+      method: "POST",
+      body: JSON.stringify(registerInput.value),
+    });
 
+    loading.value = false;
+    console.log("REs::::", res);
+  } catch (err) {
+    loading.value = false;
+    console.log("Error::::", err);
+  }
 }
 
 // Watch errors reactively
-watch(() => v$.value.email.$errors, (newVal) => {
-  console.log("x::::", JSON.parse(JSON.stringify(newVal)))
-})
-
-
+watch(
+  () => v$.value.email.$errors,
+  (newVal) => {
+    console.log("x::::", JSON.parse(JSON.stringify(newVal)));
+  }
+);
 </script>
 
 <template>
   <div class="min-h-screen flex items-center justify-center bg-gray-50">
     <div class="w-full max-w-sm bg-white p-8 rounded-xl shadow">
-       
       <!-- Title -->
       <h2 class="text-center text-2xl font-bold text-gray-900">Sign up</h2>
       <!-- Form -->
       <form class="mt-6 space-y-4">
-      <!-- Email -->
-      <FormError :errors="v$.email.$errors">
-        <BaseInput v-model="registerInput.email" :type="'text'" :placeholder="'Enter user name '" />
-      </FormError>
+        <!-- Email -->
+        <FormError :errors="v$.email.$errors">
+          <BaseInput
+            v-model="registerInput.email"
+            :type="'text'"
+            :placeholder="'Enter user name '"
+          />
+        </FormError>
         <!-- Password -->
         <FormError :errors="v$.email.$errors">
-          <BaseInput v-model="registerInput.password" :type="'password'" :placeholder="'Enter password '" />
+          <BaseInput
+            v-model="registerInput.password"
+            :type="'password'"
+            :placeholder="'Enter password '"
+          />
         </FormError>
         <!-- Button -->
-         <BaseBtn :loading="true" :label="'Signup'" @click="submitInput"/>
+        <BaseBtn :loading="loading" :label="'Signup'" @click="submitInput" />
       </form>
 
       <!-- Link -->
